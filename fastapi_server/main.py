7879054945c7adc647c553fbd1af8d31964d071f
kim hyung sun <skyupdown543@naver.com>
2025-07-27 17:39:04 +0900

    ecg_tenp fastapi server add

--- fastapi_server/main.py
+++ fastapi_server/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
-from ecg_model import predict_ecg_from_array
+from ecg_temp_model import predict_ecg_from_array
 
 app = FastAPI()
 
